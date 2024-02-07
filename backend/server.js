503482bcc1cb509f66ed7ddfc8e556a6028cbc92
Nilaxsh <nilaxsakana@gmail.com>
2024-02-07 11:45:16 +0530

    todoo

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express'); 
+const cors = require('cors'); 
+const mongoose = require('mongoose'); 
+const Todo = require("./models/todo");
+const port = 4001;
+require('dotenv').config(); 
+
+//Execute express 
+const app = express();
+
+const connectionString = process.env.DATABASE_URL; 
+mongoose.connect(connectionString)
+        .then(() => console.log('Connected to the database…')) 
+        .catch((err) => console.error('Connection error:', err));
+
+//Middlewares
+app.use(express.json()); 
+app.use(cors()); 
+
+ 
+app.get('/todo', async (req, res) => { 
+    const todos = await Todo.find(); 
+    res.json(todos); });
+
+app.post('/todo/new', async (req,res) => {
+    const newTask = await Todo.create(req.body);
+    res.status(201).json({newTask})
+})   
+
+app.delete('/todo/delete/:id', async(req,res)=>{
+    const result = await Todo.findByIdAndDelete(req.params.id)
+    res.json(result)
+})
+
+
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`)); 
